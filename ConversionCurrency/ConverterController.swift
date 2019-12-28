7487b005540c678ed47933d9e24707b5cbdc0cc8
melies <melieskubrick@gmail.com>
2019-12-27 23:51:46 -0300

    Login Layout

---
+++ ConversionCurrency/ConverterController.swift
@@ -0,0 +1,30 @@
+//
+//  ConverterController.swift
+//  ConversionCurrency
+//
+//  Created by Melies Kubrick on 27/12/19.
+//  Copyright © 2019 Melies. All rights reserved.
+//
+
+import UIKit
+
+class ConverterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
